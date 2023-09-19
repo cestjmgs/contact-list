4dd43fae35176e0d48fb2f743a7d368f992545a5
JuliGomez <juliana.go.ma.sa@gmail.com>
2023-09-19 15:05:47 -0500

    cree la lista de contactos y las funciones

---
+++ app.js
@@ -0,0 +1,33 @@
+const listaDeContactos = [
+    "John Lennon",
+    "Paul McCartney",
+    "Ringo Star",
+    "George Harrison",
+]
+
+function nuevoContacto(nombreCompleto) {
+    listaDeContactos.push(nombreCompleto);
+}
+
+
+
+function eliminarContacto (borrarNombre) {
+    const contacto = listaDeContactos.indexOf(borrarNombre); 
+    if (contacto !== -1) {
+        listaDeContactos.splice(contacto, 1);
+    }
+}
+
+
+
+function imprimirLista () {
+    for (let i = 0; i < listaDeContactos.length; i++) {
+        console.log (listaDeContactos[i]);
+}
+};
+
+nuevoContacto ("Yoko Ono"); 
+eliminarContacto ("Paul McCartney"); 
+imprimirLista ();
+
+
