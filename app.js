f0e8cbd867786322d062bdefafe4a9032439b300
JuliGomez <juliana.go.ma.sa@gmail.com>
2023-09-28 14:37:01 -0500

    creé rama project-2, modifiqué las funciones para almacenar nueva info y modifiqué función eliminar contacto

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
-const listaDeContactos = [
-    "John Lennon",
-    "Paul McCartney",
-    "Ringo Star",
-    "George Harrison",
-]
+const listaDeContactos = []
 
-function nuevoContacto(nombreCompleto) {
+function nuevoContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
+    const nombreCompleto = {
+        id: id,
+        nombres: nombres,
+        apellidos: apellidos,
+        telefono: telefono,
+        ciudad: ciudad,
+        direccion: direccion,
+
+    }; 
+
     listaDeContactos.push(nombreCompleto);
     
 }
 
 
-
-function eliminarContacto (borrarNombre) {
-    const contacto = listaDeContactos.indexOf(borrarNombre); 
-    if (contacto !== -1) {
-        listaDeContactos.splice(contacto, 1);
+function eliminarContacto (id) {
+    const indice = listaDeContactos.findIndex(contacto => contacto.id === id)
+    if (indice !== -1) {
+        listaDeContactos.splice(indice, 1);
     }
-}
+};
 
 function imprimirLista () {
-    for (let i = 0; i < listaDeContactos.length; i++) {
-        console.log(listaDeContactos[i]);
-}}; 
+    listaDeContactos.forEach( Contacto => {
+        console.log(`id: ${Contacto.id}`);
+        console.log(`nombres: ${Contacto.nombres}`);
+        console.log(`apellidos: ${Contacto.apellidos}`);
+        console.log(`telefono: ${Contacto.telefono}`);
+        console.log(`ciudad: ${Contacto.ciudad}`);
+        console.log(`dirreccion: ${Contacto.direccion}`);
+    });
+
+}; 
 
 
-nuevoContacto ("Yoko Ono"); 
-eliminarContacto ("Paul McCartney"); 
+nuevoContacto (1, "John", "Lennon", 77701842032, "Nueva York", "West 72nd Street",); 
+nuevoContacto (2, "Paul", "McCartney", 1828458129, "Los Angeles", "St John's Wood", ); 
+nuevoContacto (3, "George", "Harrison", "1901934781", "Liverpool", "957 Lancashire",);
+nuevoContacto (4, "Ringo", "Starr", "736192641", "Los Angeles", "Beverly Hills",);
+
+eliminarContacto(4);
 imprimirLista ();
+
